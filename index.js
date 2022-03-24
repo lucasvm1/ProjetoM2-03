a3f790650975dab77b7b275cf08dbc80a5b97873
LucasVM1 <lucas.vianam@outlook.com>
2022-03-24 14:07:29 -0300

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from 'path'
+const app = express();
+
+const __dirname = path.resolve(path.dirname(''))
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+
+const port = process.env.PORT || 3000;
+
+// -- //
+
