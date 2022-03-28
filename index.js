643c5c000caa083a4b7f879957d20e7f62545e55
LucasVM1 <lucas.vianam@outlook.com>
2022-03-28 20:00:57 -0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 import express from "express";
-import path from 'path'
-const app = express();
-
-const __dirname = path.resolve(path.dirname(''))
-
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, "public")))
 
 const port = process.env.PORT || 3000;
+const app = express();
 
-// -- //
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-  })
-    
-  app.listen(port, (req, res) =>{
-    })
+app.listen(port, (req, res) =>{
+  console.log(`Rodando na porta ${port}`)
+})
+
