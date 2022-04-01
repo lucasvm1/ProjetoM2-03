715597f34b4af3b9cbe5958a8a93ee057aea4a93
LucasVM1 <lucas.vianam@outlook.com>
2022-04-01 19:22:45 -0300

    .

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from 'express';
-import { routes } from './src/routes/routes.js';
-import path from 'path'
-import dotenv from 'dotenv'
+import express from "express";
+import { routes } from "./src/routes/routes.js";
+import path from "path";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
-const __dirname = path.resolve(path.dirname('')) 
+const __dirname = path.resolve(path.dirname(""));
 
-app.use(routes)
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(routes);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, (req, res) => {
-    console.log(`Rodando na porta ${port}`)
-})
-
+  console.log(`Rodando na porta ${port}`);
+});
--- src/controller/MusicasController.js
+++ src/controller/MusicasController.js
@@ -1,27 +1,23 @@
-import { variados } from '../model/musicas.js'
+import { variados } from "../model/musicas.js";
 
 export const getIndex = async (req, res) => {
-    try{
-        const musicas = await variados.findAll ();
-        res.render('index.ejs', {
-            musicas
-    })
-    }
-        catch(err){
-            console.log(err.message)
-    }
-}
+  try {
+    const musicas = await variados.findAll();
+    res.render("index.ejs", {
+      musicas,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 export const getDetalhes = async (req, res) => {
-    try{
-        const musica = await variados.findByPk(req.params.id)
-        res.render('detalhes.ejs',{
-            musica
-        })
-    }
-
-    catch(err)
-    {
-        console.log(err.message)
-    }
-}
+  try {
+    const musica = await variados.findByPk(req.params.id);
+    res.render("detalhes.ejs", {
+      musica,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
